c98c5a42fd56de5b789a355be245da206e8a3f8b
RytnixR <rajeevkhanduri55@gmail.com>
2026-03-15 01:44:45 +0530

    javascript if else

---
+++ Frontend/java script/lecture 56/index.js
@@ -0,0 +1,29 @@
+console.log("conditional tutorial")
+
+let a = 3;
+let grace = 2;
+
+a += grace
+console.log(a)
+
+console.log(a + grace)
+console.log(a - grace)
+console.log(a / grace)
+console.log(a * grace)
+console.log(a ** grace)
+console.log(a % grace)
+
+if(a>18){
+    console.log("You can drive");
+}
+else if(a == 0)(
+    console.log("can't walk")
+)
+else{
+    console.log("You can't drive ");
+    
+}
+a = 6;
+b = 8;
+let c = a> b ? (a - b) : (b - a);
+console.log(c)
