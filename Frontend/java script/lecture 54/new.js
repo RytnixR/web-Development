97c4765b235bdcac2459804f3aca1d64f89fe3f9
RytnixR <rajeevkhanduri55@gmail.com>
2026-03-14 14:55:21 +0530

    javascript started

---
+++ Frontend/java script/lecture 54/new.js
@@ -0,0 +1,4 @@
+console.log("Hello")
+console.log("Learning backend js with nodejs")
+console.log("it has some less feature then browser js but had some new feature like file read and etc")
+console.log("code is looking like a wow..") 
---
+++ Frontend/java script/lecture 54/script.js
@@ -0,0 +1,22 @@
+alert("hahha got it");
+
+console.log("code is running..")
+console.log("code is also running..")
+console.log("code is looking like a wow..")
+
+var a = prompt("Enter your no.")
+var isTrue =confirm("Are you sure you want to leave this page and burn your computer")
+
+if(isTrue){
+    console.log("blasting")
+}
+else{
+    console.log("not blasting")
+}
+ 
+console.log("Your no. is " +a)
+
+document.title = "Good to go"
+
+// document.body.style.backgroundColor = "red"
+
