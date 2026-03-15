3d340d78b7fddb0c9c072ac2da15b779088c5bf3
RytnixR <rajeevkhanduri55@gmail.com>
2026-03-15 16:07:40 +0530

    function

---
+++ Frontend/java script/lecture 58/index.js
@@ -0,0 +1,25 @@
+function intro_q() {
+
+    console.log("hey")
+    console.log("Are you good?")
+    console.log("Nice to see you")
+    console.log("got any plans?")
+}
+
+function sum(a, b) {
+    // console.log(a + b);
+    return a + b;
+}
+// similar to c language can check in my c language playlist
+intro_q()
+result = sum(4, 5);
+console.log("The sum of these two no. is: ", result)
+
+// new from c language arrow function
+const func1 = (x) => {
+    console.log("I am an arrow function", x);
+    
+}
+func1(34)
+func1(88)
+func1(957);
