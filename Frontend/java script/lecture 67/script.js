79156b1eac1919bfbc8996013c4f5d1755749d1f
RytnixR <rajeevkhanduri55@gmail.com>
2026-03-17 13:08:38 +0530

    JavaScript DOM's parent, child and sibling

---
+++ Frontend/java script/lecture 67/script.js
@@ -0,0 +1,25 @@
+console.log("Hello World")
+/*
+all run in browser
+
+document.body
+
+document.body.childNodes
+
+document.body.childNodes[0]
+
+document.body.childNodes[1]
+
+document.body.childNodes[1].childNodes
+
+let cont = document.body.childNodes[1]
+undefined
+cont
+cont.firstChild
+cont.lastChild
+
+cont.firstElementchild
+cont.lastElementchild
+cont.lastElementchild.style.backgroundcolor = "green"
+cont.lastElementchild.parentElement
+*/
