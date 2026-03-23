a7882093423c4b08b69011055c008535effcb9c4
RytnixR <rajeevkhanduri55@gmail.com>
2026-03-23 16:00:30 +0530

    lecture complete and learn hosting for var

--- Frontend/java script/lecture 82/script.js
+++ Frontend/java script/lecture 82/script.js
@@ -6,6 +6,10 @@ async function  sleep() {
     })
 }
 
+function sum(a, b, c){
+    return a + b + c
+}
+
 (async function main() {
         
     // let a = await sleep()
@@ -13,6 +17,23 @@ async function  sleep() {
     // let b = await sleep()
     // console.log(a)
 
-    let [x,y, ...rest] = [1, 5, 7, 8, 9, 10]
-    console.log(x,y, rest)
+    // let [x,y, ...rest] = [1, 5, 7, 8, 9, 10]
+    // console.log(x,y, rest)
+
+    let obj  = {
+        a : 1,
+        b : 2,
+        c : 3
+    }
+
+    let {a, b} = obj
+    console.log(a, b)
+
+    let arr = [1, 4, 6]
+    // console.log(arr[0] + arr[1] + arr[2]) // same can be written as below line
+    console.log(sum(arr[0], arr[1], arr[2]))  // or can be as writen below
+    console.log(sum(...arr))
+
+    // hosting  declaration on var move at the top of the block but assignment remains at same place
+
 })()
