192a61f0d7f08a462fb8bafa4ea6083c834cbc64
RytnixR <rajeevkhanduri55@gmail.com>
2026-07-31 23:37:34 +0530

    ComanJs and EcmaScript modules

---
+++ Backend/Java script/lecture 84/main.js
@@ -0,0 +1,18 @@
+// const { createServer } = require('node:http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1>Hello World</h1>');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const a = require("./mymodule2.js")
+
+console.log(a, __dirname, __filename)
